da743c7fc6440e35e85dff259a3119fc76997faf
Bernhard Richter <bernhard.richter@gmail.com>
2019-02-11 12:27:17 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.DependencyModel;
+namespace DependencyModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var fileStream = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"..","..", "..", "obj", "project.assets.json"), FileMode.Open))
+            {
+                using (var dependencyReader = new DependencyContextJsonReader())
+                {
+                    var context = dependencyReader.Read(fileStream);
+                    foreach(var rl in context.RuntimeLibraries)
+                    {
+                        Console.WriteLine(rl.Name);
+                    }
+                }
+            }
+        }
+    }
+}
